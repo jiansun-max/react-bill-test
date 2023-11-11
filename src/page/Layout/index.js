1cfb3b0fa444eafbe40c9ed9b9b65c0b3cdd8d7d
WindyGraph <137336036@qq.com>
2023-11-11 16:08:28 +0800

    Implement tab bar in Layout component

--- src/page/Layout/index.js
+++ src/page/Layout/index.js
@@ -1,9 +1,32 @@
+import { TabBar } from 'antd-mobile'
 import { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
-import { Outlet } from 'react-router-dom'
-import { Button } from 'antd-mobile'
-
 import { getBillList } from '@/store/modules/billStore'
+import './index.scss'
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline,
+} from 'antd-mobile-icons'
+
+const tabs = [
+  {
+    key: '/month',
+    title: '月度账单',
+    icon: <BillOutline />,
+  },
+  {
+    key: '/new',
+    title: '记账',
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: '/year',
+    title: '年度账单',
+    icon: <CalculatorOutline />,
+  },
+]
 
 const Layout = () => {
   const dispatch = useDispatch()
@@ -11,13 +34,26 @@ const Layout = () => {
     dispatch(getBillList())
   }, [dispatch])
 
+  const navigate = useNavigate()
+  const switchRoute = (path) => {
+    navigate(path)
+  }
+
   return (
-    <div>
-      <Outlet />
-      Layout
-      <Button color='primary'>测试全局</Button>
-      <div className='purple-theme'>
-        <Button color='primary'>测试局部</Button>
+    <div className='layout'>
+      <div className='container'>
+        <Outlet />
+      </div>
+      <div className='footer'>
+        <TabBar onChange={switchRoute}>
+          {tabs.map((item) => (
+            <TabBar.Item
+              key={item.key}
+              icon={item.icon}
+              title={item.title}
+            />
+          ))}
+        </TabBar>
       </div>
     </div>
   )
--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       {
-        index: true,
+        path: 'month',
         element: <Month />,
       },
       {
