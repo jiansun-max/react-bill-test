98d1fb677c0f4dab078f1d99e5e34edb1a812c28
WindyGraph <137336036@qq.com>
2023-11-11 15:52:56 +0800

    Add Redux store and fetch bill list

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import './index.css'
-import router from './router'
+import { Provider } from 'react-redux'
 
+import './index.css'
 import './theme.css'
 
+import router from './router'
+import store from './store'
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<RouterProvider router={router} />)
+root.render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+)
--- src/page/Layout/index.js
+++ src/page/Layout/index.js
@@ -1,7 +1,16 @@
-import { Button } from 'antd-mobile'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { Outlet } from 'react-router-dom'
+import { Button } from 'antd-mobile'
+
+import { getBillList } from '@/store/modules/billStore'
 
 const Layout = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getBillList())
+  }, [dispatch])
+
   return (
     <div>
       <Outlet />
