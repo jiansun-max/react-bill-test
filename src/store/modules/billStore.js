869e1a2b2d79716ecf8133a88f8fba0ddeea5679
sunjian <sujian_xm.cicdi@chinaccs.cn>
2023-11-29 17:21:19 +0800

    init

--- src/store/modules/billStore.js
+++ src/store/modules/billStore.js
@@ -1,35 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit'
-import axios from 'axios'
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const billStore = createSlice({
-  name: 'bill',
+  name: "bill",
   initialState: {
     billList: [],
   },
   reducers: {
     setBillList(state, action) {
-      state.billList = action.payload
+      state.billList = action.payload;
     },
     addBill(state, action) {
-      state.billList.push(action.payload)
+      state.billList.push(action.payload);
     },
   },
-})
+});
 
-const { setBillList, addBill } = billStore.actions
+const { setBillList, addBill } = billStore.actions;
 const getBillList = () => {
   return async (dispatch) => {
-    const res = await axios.get('http://localhost:8888/ka')
-    dispatch(setBillList(res.data))
-  }
-}
+    const res = await axios.get("http://localhost:8888/ka");
+    dispatch(setBillList(res.data));
+  };
+};
 const addBillList = (data) => {
   return async (dispatch) => {
-    const res = await axios.post('http://localhost:8888/ka', data)
-    dispatch(addBill(res.data))
-  }
-}
-export { getBillList, addBillList }
+    const res = await axios.post("http://localhost:8888/ka", data);
+    dispatch(addBill(res.data));
+  };
+};
+export { getBillList, addBillList };
 
-const reducer = billStore.reducer
-export default reducer
+const reducer = billStore.reducer;
+export default reducer;
